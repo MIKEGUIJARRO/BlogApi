7e864fa619a52bb0d081d7c10578c64378e8b6a5
Mike Guijarro <a01612042@itesm.mx>
2022-11-03 23:04:30 -0600

    Add helpers utility file

---
+++ src/util/helpers.js
@@ -0,0 +1,10 @@
+module.exports.isObject = (obj) => {
+    if (typeof obj === 'object' &&
+        !Array.isArray(obj) &&
+        obj !== null
+    ) {
+        return true;
+    }
+
+    return false;
+}
